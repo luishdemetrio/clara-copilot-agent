92912ab1978ed2de6970c6c10f331d37cfc0bb57
Luis Demetrio <49683427+luishdemetrio@users.noreply.github.com>
2026-08-03 12:12:09 -0300

    Delete src/Clara.API directory

--- src/Clara.API/Models/LicenseCountsDto.cs
+++
@@ -1,8 +0,0 @@
-namespace Clara.API.Models;
-
-public class LicenseCountsDto
-{
-    public int TotalLicenses { get; set; }
-    public int AssignedLicenses { get; set; }
-    public int AvailableLicenses { get; set; }
-}
